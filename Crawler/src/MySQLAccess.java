217c7acd173182b59a945b9fc62390f8b75a33a9
OmarDesoky <omar.ahmed98330@gmail.com>
2020-05-23 23:41:00 +0200

    [modified] changed retreieve function && [added] insertrank function (needs place holder)

--- Crawler/src/MySQLAccess.java
+++ Crawler/src/MySQLAccess.java
@@ -18,37 +18,40 @@ public class MySQLAccess {
 
   final private String host = "127.0.0.1";
   final private String user = "root";
-  final private String passwd = "";
+  final private String passwd = "123456789";
   
-  public void readDataBase() throws Exception {
+  public ResultSet readDataBase(String s) throws Exception {
     try {
       // This will load the MySQL driver, each DB has its own driver
-      Class.forName("com.mysql.jdbc.Driver");
+      Class.forName("com.mysql.cj.jdbc.Driver");
       
       // Setup the connection with the DB
       connect = DriverManager
-          .getConnection("jdbc:mysql://" + host + "/crawler_database?"
+          .getConnection("jdbc:mysql://" + host + "/Crawler_database?"
               + "user=" + user + "&password=" + passwd );
       
       //reading from the crawler database
       statement = connect.createStatement();
-      ResultSet resultSet = statement.executeQuery("select * from crawler_database.crawler_table");
-      
+      ResultSet resultSet = statement.executeQuery(s);
+      return resultSet;
+//      while(resultSet.next())
+//      {
+//    	  System.out.println(resultSet.getString(1)+" "+resultSet.getString(2));
+//      }
       //select the first row
-      resultSet.absolute(1);
-      
-      System.out.println(resultSet.getString(1)); // kda ana hatb3 el link ely fe row one
+//      resultSet.absolute(10);
+//      System.out.println(resultSet.getString(1)); // kda ana hatb3 el link ely fe row one
       
       
       //select the third row
-      resultSet.absolute(3);
+//      resultSet.absolute(3);
       
-      System.out.println(resultSet.getString(3)); // kda ana hatb3 el image sources ely fel third row
+//      System.out.println(resultSet.getString(3)); // kda ana hatb3 el image sources ely fel third row
       
     } catch (Exception e) {
         throw e;
       } finally {
-        close();
+//        close();
       }
   } 
       //writeResultSet(resultSet);
@@ -103,11 +106,11 @@ public class MySQLAccess {
 	  PreparedStatement preparedStatement = null;
 	  try {
 	      // This will load the MySQL driver, each DB has its own driver
-	      Class.forName("com.mysql.jdbc.Driver");
+	      Class.forName("com.mysql.cj.jdbc.Driver");
 	      
 	      // Setup the connection with the DB
 	      connect = DriverManager
-	          .getConnection("jdbc:mysql://" + host + "/crawler_database?"
+	          .getConnection("jdbc:mysql://" + host + "/Crawler_database?"
 	              + "user=" + user + "&password=" + passwd );
 	    } catch (Exception e) {
 	        System.out.println("database error");
@@ -124,6 +127,33 @@ public class MySQLAccess {
       preparedStatement.close();
       connect.close();
     }
+  public void saveRank(String values) throws SQLException {
+	  Connection connect = null;
+	  PreparedStatement preparedStatement = null;
+	  try {
+	      // This will load the MySQL driver, each DB has its own driver
+	      Class.forName("com.mysql.cj.jdbc.Driver");
+	      
+	      // Setup the connection with the DB
+	      connect = DriverManager
+	          .getConnection("jdbc:mysql://" + host + "/Crawler_database?"
+	              + "user=" + user + "&password=" + passwd );
+	    } catch (Exception e) {
+	        System.out.println("database error");
+	      } finally {
+	        close();
+	      }
+	  statement = connect.createStatement();
+	  statement.executeUpdate("DROP TABLE IF EXISTS POPULARITY_RANK;");
+	  statement.executeUpdate("CREATE TABLE POPULARITY_RANK(LINK VARCHAR(256) NOT NULL PRIMARY KEY, POPULARITY_SCORE DOUBLE NOT NULL);");
+	  
+	  preparedStatement = connect.prepareStatement("INSERT INTO POPULARITY_RANK(LINK,POPULARITY_SCORE) VALUES "+values);
+//      preparedStatement.setString(1, values);
+      preparedStatement.executeUpdate();
+//      preparedStatement.close();
+//      connect.close();
+    }
+  
  
   // You need to close the resultSet
   private void close() {
