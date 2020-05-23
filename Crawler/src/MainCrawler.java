28153156393f38391393dc31b0a88f71317abb88
OmarDesoky <omar.ahmed98330@gmail.com>
2020-05-23 23:41:45 +0200

    merge conflict

--- Crawler/src/MainCrawler.java
+++ Crawler/src/MainCrawler.java
@@ -1,5 +1,5 @@
 // Adapted from http://www.vogella.com/tutorials/MySQLJava/article.html
-public class Main {
+public class MainCrawler {
   public static void main(String[] args) throws Exception {
     MySQLAccess dao = new MySQLAccess();
     dao.readDataBase();
