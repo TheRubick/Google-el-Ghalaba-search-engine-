90458c4ca16b7a0e5d66ca90b02474cca6a070b6
OmarDesoky <omar.ahmed98330@gmail.com>
2020-06-01 10:53:23 +0200

    [added] ImageRank file :)

---
+++ server [merge]/src/main/java/com/example/demo/ImageRank.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class ImageRank  {
+    private String words;
+    private MySQLAccess db;
+    private int numImgs;
+    void startRank(ArrayList<String> queryProcessed) throws Exception
+    {
+        db = new MySQLAccess();
+        words = "(";
+        for (int i = 0; i < queryProcessed.size() - 1; i++) {
+            words += "'" + queryProcessed.get(i) + "',";
+        }
+        words += "'" + queryProcessed.get(queryProcessed.size() - 1) + "')";
+        String query = "SELECT DISTINCT(`image_link`), `title` FROM `Images` WHERE WORD IN " + words;
+        ResultSet queryResult = db.readDataBase(query);
+
+        queryResult.last();
+        numImgs =  queryResult.getRow();
+        ServerAPI.Img[] toFrontEnd = new ServerAPI.Img[numImgs];
+        queryResult.beforeFirst();
+        int i =0;
+        while (queryResult.next()) {
+            String link = queryResult.getString(1);
+            String title= queryResult.getString(2);
+            ServerAPI.Img element = new ServerAPI.Img(title,link);
+            toFrontEnd[i] = element ;
+            i++;
+        }
+    }
+}
